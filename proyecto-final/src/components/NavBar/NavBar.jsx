185784954f82eff36bb6df8bca0f3b4a79e877f6
Belen <blnmolina@gmail.com>
2022-04-14 13:31:04 -0300

    navBar

---
+++ proyecto-final/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./NavBar.css";
+
+
+function NavBar(props) {
+    return (
+        <div>
+            <ul className="nav">
+                <li>ASG CAR DETAILING</li>
+                <a href="#">Inicio</a>
+                <a href="#">Servicios</a>
+                <a href="#">Quienes Somos</a>
+                <a href="#">Capacitaciones</a>
+                <a href="#">Contacto</a>
+                
+
+
+
+            </ul>
+        </div>
+    );
+
+
+
+
+}
+export default NavBar;
