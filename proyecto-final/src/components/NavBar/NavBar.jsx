846b1b9c29dd0021d5d7146a61587c44e2d82d2d
Belen <blnmolina@gmail.com>
2022-04-18 12:32:26 -0300

    cambios

--- proyecto-final/src/components/NavBar/NavBar.jsx
+++ proyecto-final/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import "./NavBar.css";
+import asg from "./assets/asg.jpg";
+import carrito from "./assets/carrito.png";
 
 
 function NavBar(props) {
     return (
         <div>
-            <ul className="nav">
-                <li>ASG CAR DETAILING</li>
-                <a href="#">Inicio</a>
-                <a href="#">Servicios</a>
-                <a href="#">Quienes Somos</a>
-                <a href="#">Capacitaciones</a>
-                <a href="#">Contacto</a>
-                
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <img  class="logo"src={asg} alt="" />
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Quienes Somos</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Capacitaciones</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                    <img class="carrito" src={carrito} alt="" />
+                    
+                </ul>
+                </div>
+            </div>
+            </nav>
 
-
-
-            </ul>
-        </div>
+            
+            </div>
+        
     );
 
 
