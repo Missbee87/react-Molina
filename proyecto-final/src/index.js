b25c3c54d996b7f2c243b967428a131a2ef709db
Belen <blnmolina@gmail.com>
2022-05-17 12:26:22 -0300

    firebase

--- proyecto-final/src/index.js
+++ proyecto-final/src/index.js
@@ -5,8 +5,17 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import { CartContextProvider } from './store/Cart-context';
+import { initializeApp } from "firebase/app";
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDmMBUGlvLEoGmBrF549O9Zf4aueU_ZZgg",
+  authDomain: "proyecto-final-molina.firebaseapp.com",
+  projectId: "proyecto-final-molina",
+  storageBucket: "proyecto-final-molina.appspot.com",
+  messagingSenderId: "1077091744472",
+  appId: "1:1077091744472:web:d8d9eb1afd9a85a8c2f5ad"
+};
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
